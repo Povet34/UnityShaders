0d94cca84e10666fe518555fe1d7dc6682228ff3
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-05-27 12:02:55 +0900

    한축(z) 행렬 회전

---
+++ Shaders/Assets/MyTestbed/Rendering1/Transformation/RotationTransformation.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace CLC.Rendering
+{
+    public class RotationTransformation : Transformation
+    {
+        public Vector3 rotation;
+
+        public override Vector3 Apply(Vector3 point)
+        {
+            float radZ = rotation.z * Mathf.Deg2Rad;
+            float sinZ = Mathf.Sin(radZ);
+            float cosZ = Mathf.Cos(radZ);
+
+            return new 
+            (
+                point.x * cosZ - point.y * sinZ,
+                point.x * sinZ + point.y * cosZ,
+                point.z
+            );
+        }
+    }
+}
